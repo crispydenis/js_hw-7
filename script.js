adb8cd4bfa63c26fe09143f95536d28a521f0c25
Denis <9117008060d@gmail.com>
2023-11-15 17:18:09 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -5,8 +5,6 @@
 двигателя, вкл./выкл. передачи (вперед, назад, нейтральная), движение вперед и назад
 (ввод информации о скорости движения), расчет пройденных километров. */
 
-
-
 let Lamp = function(w, t, tt) {
     this.status = false;
 
@@ -37,64 +35,31 @@ lamp1.onOff()
 // console.log(lamp2);
 // console.log(lamp3);
 
+
+
+
 let car = function(){
 
 }
 
 // Конструктор объекта "Автомобиль"
-function Car(brand, number) {
-    // Внутренние свойства
+function Car(brand, model, number) {
     this.brand = brand;
+    this.model = model;
     this.number = number;
     this.engineOn = false;
-    this.transmission = "нейтральная";
+    this.transmission = "parking";
     this.speed = 0;
     this.distanceTraveled = 0;
-  
-    // Метод для включения/выключения двигателя
-    this.toggleEngine = function () {
-      this.engineOn = !this.engineOn;
-      console.log(`Двигатель ${this.engineOn ? "включен" : "выключен"}`);
-    };
-  
-    // Метод для включения/выключения передачи
-    this.toggleTransmission = function () {
-      if (this.engineOn) {
-        this.transmission =
-          this.transmission === "вперед" ? "назад" : "вперед";
-        console.log(`Передача: ${this.transmission}`);
-      } else {
-        console.log("Двигатель выключен. Включите двигатель.");
-      }
-    };
-  
-    // Метод для движения вперед или назад
-    this.move = function (direction, speed) {
-      if (this.engineOn) {
-        this.transmission === direction
-          ? console.log(`Движение ${direction} со скоростью ${speed} км/ч`)
-          : console.log("Переключите передачу.");
-        this.speed = speed;
-        this.distanceTraveled += speed; // Просто для примера, можно добавить точный расчет расстояния
-      } else {
-        console.log("Двигатель выключен. Включите двигатель.");
-      }
-    };
-  
-    // Метод для расчета пройденных километров
-    this.calculateDistance = function () {
-      console.log(`Пройдено километров: ${this.distanceTraveled} км`);
-    };
-  }
-  
-  // Создание экземпляра объекта "Автомобиль"
-  const myCar = new Car("Toyota", "A123BC");
-  
-  // Использование методов и свойств
-  myCar.toggleEngine(); // Включение двигателя
-  myCar.toggleTransmission(); // Включение передачи
-  myCar.move("вперед", 40); // Движение вперед со скоростью 40 км/ч
-  myCar.move("вперед", 60); // Движение вперед со скоростью 40 км/ч
-  myCar.calculateDistance(); // Расчет пройденных километров
-  myCar.toggleEngine(); // Выключение двигателя
-  
+
+    // двигатель вкл
+    this.toEngineOn = function(){
+        this.engineOn = !this.engineOn
+        console.log('двигатель вкл')
+    }
+
+
+
+}
+
+const myCar = new Car("Toyota", "A123BC");
